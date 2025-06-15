07f4ee424a25d212b93713b0b8cfbac624f7539f
abanishkhatry <khatriavanish@gmail.com>
2025-06-15 15:53:24 -0500

    Neetcode Stacks : Valid Parenthesis (E) - Completed

--- validParentheses.py
+++ validParentheses.py
@@ -1,27 +1,29 @@
-from typing import List 
-
+from typing import List
 s = "[(])"
-
+# Solution - Optimal Approach 
+# Time Complexity : O(n), where n is the size of input array
+# Space Complexity : O(n)
 def isValid(s: str) -> bool : 
-    stack = []
-    for i in s: 
-        if len(s) % 2 != 0 or len(s) == 0:
+    # if s is empty or odd in length. 
+    if len(s) % 2 != 0 or len(s) == 0:
             return False
-        if i in "[{(":
+    
+    stack = []
+    mapping = {')': '(', ']': '[', '}': '{'}
+    for i in s: 
+        # if opening brackets then appending them to the stack
+        if i in mapping.values:
             stack.append(i)
-            if (len(s) == len(stack)): 
-               return False 
-        elif i in "}])": 
-            if len(stack) != 0: 
-             current_high = stack.pop()
-             if i == "}" and current_high == "{": 
-                continue
-             elif (i == "]" and current_high == "["): 
-                continue
-             elif i == ")" and current_high == "(": 
-                continue
-            else: 
-                return False
-    return True
+        # if closing brackets then checking them by poping the latest bracket put into the stack that only holds
+        # latest opening brackets.    
+        elif i in mapping: 
+            if not stack or stack.pop() != mapping[i]:
+                return False 
+
+    # for valid string, checking if the stack is empty.         
+    if(len(stack) == 0) :       
+        return True
+    else : 
+       return False
 
 print(isValid(s))
